bb9004474438769b33f6fc16837ce18ff22ff5e4
canadianbees <88695497+canadianbees@users.noreply.github.com>
2021-09-09 22:56:09 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,16 @@
  *  Copyright 2021 Celina Alzenor
  */
 
-
 package org.example;
 
 public class Main
 {
     public static void main( String[] args )
     {
+        //creates the 2 numbers needed for an operation
         Operations equations = new Operations();
-
+        
+        //does subtraction, division, addition, and multiplication with the two numbers
         equations.Maths();
     }
 }
